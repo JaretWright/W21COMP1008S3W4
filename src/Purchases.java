ea4ae31dece15670f4969a89671a311bcc322dbb
Jaret Wright <hkkA223>
2021-02-10 17:05:45 -0500

    Added the Purchases class

---
+++ src/Purchases.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Purchases {
+    private ArrayList<GroceryItem> shoppingCart;
+
+    public Purchases() {
+        this.shoppingCart = new ArrayList<>();
+    }
+
+    public void addGroceryItem(GroceryItem groceryItem)
+    {
+        shoppingCart.add(groceryItem);
+    }
+}
